60e75d12007516ea03739da38c46ffc9aba38eab
dragonmux <git@dragonmux.network>
2021-09-07 16:50:33 +0100

    hdl: xilinx: ps7: begun implementing the instance logic for the PS7

--- arachne/hdl/xilinx/ps7.py
+++ arachne/hdl/xilinx/ps7.py
@@ -2,18 +2,43 @@
 
 from nmigen import *
 
+from .ps    import *
+
 __all__ = (
-	'ps7',
+	'PS7',
 )
 
-class ps7(Elaboratable):
+class PS7(Elaboratable):
 	"""Xilinx Zynq SoC PS Block
 
 	"""
-	def __init__(self):
-		pass
+	def __init__(self, *, **kwargs):
+		self._resources = {
+			'can0': kwargs.get('can0', None),
+			'can1': kwargs.get('can1', None),
+			'dma0': kwargs.get('dma0', None),
+			'dma1': kwargs.get('dma1', None),
+			'dma2': kwargs.get('dma2', None),
+			'dma3': kwargs.get('dma3', None),
+		}
 
-	def elaborate(self, _):
+	def add_resource(self, *, name, resource):
+		if name not in self._resources:
+			raise ValueError('Resource name not valid')
+		elif self._resources[name] is not None:
+			raise ValueError('Resource already assigned, refusing to reassign to a new resource')
+		self._resources[name] = resource
+
+	def elaborate(self, platform):
 		m = Module()
 
+		mio = Signal(54)
+		if platform.package.lower() == 'clg255':
+			pass
+		else:
+			pass
+
+		#Instance(
+		#	'PS7',
+		#)
 		return m
