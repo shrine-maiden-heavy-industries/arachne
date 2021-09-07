ab2588da79dab6a9c569d1c73de63a37e75d404a
dragonmux <git@dragonmux.network>
2021-09-07 23:11:50 +0100

    hdl: xilinx: ps7: Made use of the new core resource to clean up the mandatory components of the PS7 instanciation

--- arachne/hdl/xilinx/ps7/__init__.py
+++ arachne/hdl/xilinx/ps7/__init__.py
@@ -3,39 +3,40 @@
 from nmigen      import *
 
 from ...amba4.axi import *
-from .mio        import *
+from .mio         import *
+from .resources   import PS7CoreResource
 
 __all__ = (
-	'PS7',
+	'PS7', 'PS7CoreResource'
 )
 
 class PS7(Elaboratable):
 	"""Xilinx Zynq SoC PS Block
 
 	"""
-	def __init__(self, *, clk : Signal, por_n : Signal, srst_n : Signal, **kwargs):
+	def __init__(self, *, core, **kwargs):
 		self._ps_resources = {
-			'can0':    kwargs.get('can0', None),
-			'can1':    kwargs.get('can1', None),
-			'dma0':    kwargs.get('dma0', None),
-			'dma1':    kwargs.get('dma1', None),
-			'dma2':    kwargs.get('dma2', None),
-			'dma3':    kwargs.get('dma3', None),
-			'ddr':     kwargs.get('ddr', None),
-			'emac0':   kwargs.get('emac0', None),
-			'emac1':   kwargs.get('emac1', None),
-			'i2c0':    kwargs.get('i2c0', None),
-			'i2c1':    kwargs.get('i2c1', None),
-			'pjtag':   kwargs.get('pjtag', None),
-			'sdio0':   kwargs.get('sdio0', None),
-			'sdio1':   kwargs.get('sdio1', None),
-			'spi0':    kwargs.get('spi0', None),
-			'spi1':    kwargs.get('spi1', None),
-			'trace':   kwargs.get('trace', None),
-			'uart0':   kwargs.get('uart0', None),
-			'uart1':   kwargs.get('uart1', None),
-			'usb0':    kwargs.get('usb0', None),
-			'usb1':    kwargs.get('usb1', None),
+			'can0':  kwargs.get('can0', None),
+			'can1':  kwargs.get('can1', None),
+			'dma0':  kwargs.get('dma0', None),
+			'dma1':  kwargs.get('dma1', None),
+			'dma2':  kwargs.get('dma2', None),
+			'dma3':  kwargs.get('dma3', None),
+			'ddr':   kwargs.get('ddr', None),
+			'emac0': kwargs.get('emac0', None),
+			'emac1': kwargs.get('emac1', None),
+			'i2c0':  kwargs.get('i2c0', None),
+			'i2c1':  kwargs.get('i2c1', None),
+			'jtag':  kwargs.get('jtag', None),
+			'sdio0': kwargs.get('sdio0', None),
+			'sdio1': kwargs.get('sdio1', None),
+			'spi0':  kwargs.get('spi0', None),
+			'spi1':  kwargs.get('spi1', None),
+			'trace': kwargs.get('trace', None),
+			'uart0': kwargs.get('uart0', None),
+			'uart1': kwargs.get('uart1', None),
+			'usb0':  kwargs.get('usb0', None),
+			'usb1':  kwargs.get('usb1', None),
 		}
 		self._pl_resources = {
 			'axi_gp0': (
@@ -99,9 +100,7 @@ class PS7(Elaboratable):
 				name = 'axi_hp3'
 			),
 		}
-		self._clk = clk
-		self._por_n = por_n
-		self._srst_n = srst_n
+		self._core = core
 
 	def add_resource(self, *, name, resource) -> None:
 		if name not in self._ps_resources:
@@ -135,9 +134,9 @@ class PS7(Elaboratable):
 
 		Instance(
 			'PS7',
-			i_PSCLK = self._clk,
-			i_PSPORB = self._por_n,
-			i_PSSRSTB = self._srst_n,
+			i_PSCLK = self._core.clk.i,
+			i_PSPORB = self._core.por_n.i,
+			i_PSSRSTB = self._core.srst_n.i,
 			**perif_map,
 		)
 		return m
