054375703185ff2ad09d382408357650a0d5dd1a
Aki Van Ness <aki@lethalbit.net>
2021-09-07 12:07:39 -0400

    hdl: xilinx: added the empty resources for the ps8 block

--- arachne/hdl/xilinx/ps8.py
+++ arachne/hdl/xilinx/ps8.py
@@ -11,8 +11,17 @@ class PS8(Elaboratable):
 	"""Xilinx Zynq UltraScale+ MPSoC PS Block
 
 	"""
-	def __init__(self):
-		pass
+	def __init__(self, *, **kwargs):
+		self._resources = {
+
+		}
+
+	def add_resource(self, *, name, resource):
+		if name not in self._resources:
+			raise ValueError('Resource name not valid')
+		elif self._resources[name] is not None:
+			raise ValueError('Resource already assigned, refusing to reassign to a new resource')
+		self._resources[name] = resource
 
 	def elaborate(self, _):
 		m = Module()
