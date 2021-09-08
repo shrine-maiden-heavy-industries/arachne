c258289987313a0c947541e0f85801eead50b69d
Aki Van Ness <aki@lethalbit.net>
2021-09-08 04:20:38 -0400

    core: cleaned up and completed the setup.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,8 +2,6 @@
 import os, sys
 sys.path.insert(0, os.path.abspath('.'))
 
-
-
 import arachne
 
 project = 'Arachne'
--- setup.py
+++ setup.py
@@ -1,15 +1,41 @@
 # SPDX-License-Identifier: BSD-3-Clause
 from setuptools import setup, find_packages
 
+def vcs_ver():
+	def scheme(version):
+		if version.tag and not version.distance:
+			return version.format_with("")
+		else:
+			return version.format_choice("+{node}", "+{node}.dirty")
+	return {
+		"relative_to": __file__,
+		"version_scheme": "guess-next-dev",
+		"local_scheme": scheme
+	}
+
+def doc_ver():
+	try:
+		from setuptools_scm.git import parse as parse_git
+	except ImportError:
+		return ""
+
+	git = parse_git(".")
+	if not git:
+		return ""
+	elif git.exact:
+		return git.format_with("{tag}")
+	else:
+		return "latest"
+
+
 setup(
 	name = 'arachne',
-	# TODO
-	# use_scm_version =
+	use_scm_version = vcs_ver(),
 	author          = 'Aki \'lethalbit\' Van Ness',
 	author_email    = 'nya@catgirl.link',
 	description     = 'Toolkit for nMigen',
 	license         = 'BSD-3-Clause',
-	python_requires = '~=3.8',
+	python_requires = '>=3.6',
 	setup_requires  = [
 		'wheel', 'setuptools', 'setuptools_scm'
 	],
