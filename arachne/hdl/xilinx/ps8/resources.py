c5ec7996f497a32fa98bc2702ff3db87f9867d4d
Aki Van Ness <aki@lethalbit.net>
2021-09-11 00:11:18 -0400

    hdl: xilinx: ps8: roughly defined some of the ps8 hard resources

--- arachne/hdl/xilinx/ps8/resources.py
+++ arachne/hdl/xilinx/ps8/resources.py
@@ -26,3 +26,116 @@ def QSPIResource():
 	io = []
 
 	return PS8Resource('qspi', 0, *io, Attrs(IOSTANDARD="LVCMOS33"))
+
+def NANDResource():
+	io = []
+
+	return PS8Resource('nand', 0, *io, Attrs(IOSTANDARD="LVCMOS33"))
+
+def SDResource(num):
+	if num > 1:
+		raise ValueError(f'PS8 Only has sd0..1, not {num}')
+
+	io = []
+
+	return PS8Resource('sd', num, *io, Attrs(IOSTANDARD="LVCMOS33"))
+
+
+def CANResource(num):
+	if num > 1:
+		raise ValueError(f'PS8 Only has can0..1, not {num}')
+
+	io = []
+
+	return PS8Resource('can', num, *io, Attrs(IOSTANDARD="LVCMOS33"))
+
+def I2CResource(num):
+	if num > 1:
+		raise ValueError(f'PS8 Only has i2c0..1, not {num}')
+
+	io = []
+
+	return PS8Resource('i2c', num, *io, Attrs(IOSTANDARD="LVCMOS33"))
+
+def PJTAGResource():
+	io = []
+
+	return PS8Resource('pjtag', 0, *io, Attrs(IOSTANDARD="LVCMOS33"))
+
+def PMUResource():
+	io = []
+
+	return PS8Resource('pmu', 0, *io, Attrs(IOSTANDARD="LVCMOS33"))
+
+def CSUResource():
+	io = []
+
+	return PS8Resource('csu', 0, *io, Attrs(IOSTANDARD="LVCMOS33"))
+
+def I2CResource(num):
+	if num > 1:
+		raise ValueError(f'PS8 Only has spi0..1, not {num}')
+
+	io = []
+
+	return PS8Resource('spi', num, *io, Attrs(IOSTANDARD="LVCMOS33"))
+
+def UARTResource(num):
+	if num > 1:
+		raise ValueError(f'PS8 Only has uart0..1, not {num}')
+
+	io = []
+
+	return PS8Resource('uart', num, *io, Attrs(IOSTANDARD="LVCMOS33"))
+
+def SWDTResource(num):
+	if num > 1:
+		raise ValueError(f'PS8 Only has swdt0..1, not {num}')
+
+	io = []
+
+	return PS8Resource('swdt', num, *io, Attrs(IOSTANDARD="LVCMOS33"))
+
+def TraceResource():
+	io = []
+
+	return PS8Resource('trace', 0, *io, Attrs(IOSTANDARD="LVCMOS33"))
+
+def TCCResource(num):
+	if num > 3:
+		raise ValueError(f'PS8 Only has tcc0..3, not {num}')
+
+	io = []
+
+	return PS8Resource('tcc', num, *io, Attrs(IOSTANDARD="LVCMOS33"))
+
+def GEMResource(num):
+	if num > 3:
+		raise ValueError(f'PS8 Only has gem0..3, not {num}')
+
+	io = []
+
+	return PS8Resource('gem', num, *io, Attrs(IOSTANDARD="LVCMOS33"))
+
+def USBResource(num):
+	if num > 1:
+		raise ValueError(f'PS8 Only has usb0..1, not {num}')
+
+	io = []
+
+	return PS8Resource('usb', num, *io, Attrs(IOSTANDARD="LVCMOS33"))
+
+def PCIEResource():
+	io = []
+
+	return PS8Resource('pcie', 0, *io, Attrs(IOSTANDARD="LVCMOS33"))
+
+def DPResource():
+	io = []
+
+	return PS8Resource('dp', 0, *io, Attrs(IOSTANDARD="LVCMOS33"))
+
+def SATAResource():
+	io = []
+
+	return PS8Resource('sata', 0, *io, Attrs(IOSTANDARD="LVCMOS33"))
