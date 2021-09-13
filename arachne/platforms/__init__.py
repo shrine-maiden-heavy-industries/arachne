2700b86cd58c8408b665cde6cb3bf19ae0abbad6
dragonmux <git@dragonmux.network>
2021-09-13 03:47:18 +0100

    platforms: Fixed a typo in the import for XilinxZynq7000Platform

--- arachne/platforms/__init__.py
+++ arachne/platforms/__init__.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
-from xilinx_zynq7000 import XilinxZynq7000Platform
+from .xilinx_zynq7000 import XilinxZynq7000Platform
 
 __all__ = ('XilinxZynq7000Platform',)
