4f946296728268df14d0e67de73faa7ea3f023a2
Aki Van Ness <aki@lethalbit.net>
2021-09-27 19:03:06 -0400

    hdl: lattice: ice40: added small warmboot elaboratable wrapper

---
+++ arachne/hdl/lattice/ice40.py
@@ -0,0 +1,24 @@
+# SPDX-License-Identifier: BSD-3-Clause
+from nmigen       import *
+from nmigen.build import *
+
+__all__ = ()
+
+
+
+class ice40_warmboot(Elaboratable):
+	def __init__(self):
+		self.boot_sel    = Signal(2)
+		self.triggr_boot = Signal()
+
+	def elaborate(self, _):
+		m = Module()
+
+		m.submodules.warmboot = Instance(
+			'SB_WARMBOOT',
+			i_BOOT = self.triggr_boot,
+			i_S0   = self.boot_sel[0],
+			i_S1   = self.boot_sel[1]
+		)
+
+		return m
